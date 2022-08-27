743bf5855606f82b3086ddf24217defacb497d1f
Uche Jeffrey <uchennaekwomadu@gmail.com>
2022-08-27 12:37:21 +0100

    update

---
+++ controller/controller.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from movieListView import *
+
+app = FastAPI()
+
+def showAll():
+    movies_in_db = get_movies
+    return showAllView(movies_in_db)
+
+def start():
+    view.startView()
+ #input = raw_input() how to?
+    if input == 'y':
+        return showAll()
+    else:
+        return pass
+
+if __name__ == "__main__":
+    start()
--- movieListView.py
+++ movieListView.py
@@ -11,3 +11,16 @@ app = FastAPI()
 async def get_movies():
     for movie in movielist:
         print(movie) 
+
+
+def showAllView(movielist : List[Movie]):
+    print 'In our db, we have %i movies' % len(movielist)
+    for item in movielist:
+        print item.title
+        
+def startView():
+    print 'Welcome'
+    print 'Show all movies?[y/n]'
+
+def endView():
+    print 'Goodbye!'
