632bb06d367ed3ede7eb965fae39005ed0505841
Favour Chukwuemeka <kaosemeka@yahoo.com>
2022-09-08 15:15:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,30 +9,6 @@ import requests
 
 app = FastAPI()
 
-movielist: List[Movie] = [
-    Movie(
-        title= 'The Unholy',
-        director= 'Evan Spiliopoulos',
-        movie_genre= genre.horror,
-        age_rating= 16,
-        id= uuid4()
-    ),    
-    Movie(
-        title= 'Infinity Wars',
-        director= 'Anthony Russo, Joe Russo',
-        movie_genre= genre.action,
-        age_rating= 13,
-        id= uuid4()
-    ), 
-    Movie(
-        title= 'Uncharted',
-        director= 'Ruben Fleischer',
-        movie_genre= genre.adventure,
-        age_rating= 13,
-        id= uuid4()
-    )
-]
-
 @app.get("/myApp/path")
 async def get_movielist():
     return movielist
