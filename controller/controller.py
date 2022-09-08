acd48268f55e7c4ec26c5c6c2262d9ee9c7d2dfb
Favour Chukwuemeka <kaosemeka@yahoo.com>
2022-09-08 15:16:53 +0100

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
 from views.movieListView import *
 
-app = FastAPI()
+movielist: List[Movie] = [
+    Movie(
+        title= 'The Unholy',
+        director= 'Evan Spiliopoulos',
+        movie_genre= genre.horror,
+        age_rating= 16,
+        id= uuid4()
+    ),    
+    Movie(
+        title= 'Infinity Wars',
+        director= 'Anthony Russo, Joe Russo',
+        movie_genre= genre.action,
+        age_rating= 13,
+        id= uuid4()
+    ), 
+    Movie(
+        title= 'Uncharted',
+        director= 'Ruben Fleischer',
+        movie_genre= genre.adventure,
+        age_rating= 13,
+        id= uuid4()
+    )
+]
 
 def showAll():
     movies_in_db = get_movies()
     return showAllView(movies_in_db)
-
-def start():
-    startView()
-    if input == 'y':
-        return showAll()
-    else:
-        return endView()
-
-if __name__ == "__main__":
-    start()
